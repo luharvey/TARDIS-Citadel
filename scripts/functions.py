201e4b7a04de17153e14a8a8ccf82a13e1291788
Luke Harvey <luharvey@Lukes-MacBook-Pro.local>
2022-05-18 11:27:40 +0100

    Hiding convergence plots

--- scripts/functions.py
+++ scripts/functions.py
@@ -5,17 +5,17 @@ import pandas as pd
 def run_simulation(path, folder):
 	name = path.split(sep = '/')[-1].split(sep = '.')[0]
 
-	sim = tardis.run_tardis(path)
+	sim = tardis.run_tardis(path, show_convergence_plots = False)
 	spec = sim.runner.spectrum_virtual
 
 	with open('../synthetic_spectra/' + folder + '/' + name + '.txt', 'w') as file:
 		for j in range(len(spec.wavelength.value)):
-			file.write(str(spec.wavelength.value[j]) + ' ' + str(spec.luminosity_density_lambda.value[j]) + ' \n')\
+			file.write(str(spec.wavelength.value[j]) + ' ' + str(spec.luminosity_density_lambda.value[j]) + ' \n')
 
 def run_simulation_csv(path, folder):
 	name = path.split(sep = '/')[-1].split(sep = '.')[0]
 
-	sim = tardis.run_tardis(path)
+	sim = tardis.run_tardis(path, show_convergence_plots = False)
 	spec = sim.runner.spectrum_virtual
 
 	path = '../synthetic_spectra/' + folder + '/spectra.csv'
